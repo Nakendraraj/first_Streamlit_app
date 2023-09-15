e071a069ccddf75492ab61de605be95ba30b44a0
Nakendraraj <145164043+Nakendraraj@users.noreply.github.com>
2023-09-15 16:33:46 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-Streamlit.subtitle ('BREAKFAST MENU')
-streamlit.message ('Idli w/ vada curry')
-streamlit.message ('Ghee Podi Dosai')
-streamlit.message ('Ghee Pongal w/vada')
-streamlit.message ('Kalakki')
+Streamlit.title ('BREAKFAST MENU')
+streamlit.title ('Idli w/ vada curry')
+streamlit.title ('Ghee Podi Dosai')
+streamlit.title ('Ghee Pongal w/vada')
+streamlit.title ('Kalakki')
 
