1492162e610d948eef111a0c5812573e47496546
Nakendraraj <145164043+Nakendraraj@users.noreply.github.com>
2023-09-15 16:50:23 -0500

    Update Streamlit_app.py
    Spelling mistake with fruit list

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -12,6 +12,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Punch 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #adding a user interface to pick the fruit they want to add
-streamlit.multiselect ("Pick your own choice of fruit:", list(my_fruits_list.index))
+streamlit.multiselect ("Pick your own choice of fruit:", list(my_fruit_list.index))
 #displaying the data in tablular format
 streamlit.dataframe(my_fruit_list)
