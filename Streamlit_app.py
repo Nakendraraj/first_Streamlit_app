0199bde7ce729984466742f476fe9778b5e69570
Nakendraraj <145164043+Nakendraraj@users.noreply.github.com>
2023-09-15 16:56:33 -0500

    Update Streamlit_app.py
    Added a pre defined fruits on pick list

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -13,6 +13,6 @@ import pandas
 my_fruit_list = pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 #adding a user interface to pick the fruit they want to add
-streamlit.multiselect ("Pick your own choice of fruit:", list(my_fruit_list.index))
+streamlit.multiselect ("Pick your own choice of fruits:", list(my_fruit_list.index),['Avacado', 'Strawberries'])
 #displaying the data in tablular format
 streamlit.dataframe(my_fruit_list)
