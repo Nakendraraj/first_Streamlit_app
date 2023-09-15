81f6872a319dc25056353c6947e273a21fcdc651
Nakendraraj <145164043+Nakendraraj@users.noreply.github.com>
2023-09-15 16:48:21 -0500

    Update Streamlit_app.py
    making it an interactive model

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -11,4 +11,7 @@ streamlit.text ('🐔 Kalakki')
 streamlit.header('🍌🥭 Build Your Own Fruit Punch 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#adding a user interface to pick the fruit they want to add
+streamlit.multiselect ("Pick your own choice of fruit:" list(my_fruits_list.index))
+#displaying the data in tablular format
 streamlit.dataframe(my_fruit_list)
