e6296be6f9f9e7ef7a42e9abe098704606630936
Nakendraraj <145164043+Nakendraraj@users.noreply.github.com>
2023-09-15 16:53:23 -0500

    Update Streamlit_app.py
    As usual....made a silly mistake by typing - instead of =

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text ('🐔 Kalakki')
 streamlit.header('🍌🥭 Build Your Own Fruit Punch 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list - my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 #adding a user interface to pick the fruit they want to add
 streamlit.multiselect ("Pick your own choice of fruit:", list(my_fruit_list.index))
 #displaying the data in tablular format
