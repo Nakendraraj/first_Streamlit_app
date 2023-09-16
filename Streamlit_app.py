e4f882d1251d91318150259cc085c89d818ecfb9
Nakendraraj <145164043+Nakendraraj@users.noreply.github.com>
2023-09-16 01:25:44 -0500

    Update Streamlit_app.py
    comment issue

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -24,19 +24,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+
 # New section to pull fruity vice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-# new entries for nested if else
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-else:
-    fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-    
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
